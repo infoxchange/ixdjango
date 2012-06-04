e0acb28954eeece79dbde9c61013c7824408cbaf
Ozgur Susoy <ozgur@infoxchange.net.au>
2012-06-04 10:20:46 +1000

    idebugging

--- setup.py
+++ setup.py
@@ -3,14 +3,15 @@ from distutils.core import setup
 # find_packages comes in handy for deep trees. see
 # http://bruno.im/2010/may/05/packaging-django-reusable-app/
 #
-from setuptools import find_packages
+#from setuptools import find_packages
 
 setup(
     name='IXDjango',
     version='0.1.0',
     author='IXA dev team',
     author_email='development@infoxchange.net.au',
-    packages=find_packages(),
+    #packages=find_packages(),
+    packages=['ixdjango'],
     url='',
     license='',
     description='Bunch of tools useful to all IX Django projects.',
