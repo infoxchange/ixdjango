6708f847030fc0cedcb01133491a36733e6a2551
Ozgur Susoy <ozgur@infoxchange.net.au>
2012-06-12 13:55:46 +1000

    All packaged up for PyPI. refs #9705

--- setup.py
+++ setup.py
@@ -2,19 +2,17 @@
 Package configuration file
 """
 from distutils.core import setup
-#
-# find_packages comes in handy for deep trees. see
-# http://bruno.im/2010/may/05/packaging-django-reusable-app/
-#
 from setuptools import find_packages
 
 setup(
     name='IXDjango',
     version='0.1.0',
-    author='IXA dev team',
-    author_email='development@infoxchange.net.au',
+    author='Infoxchanhe Australia dev team',
+    author_email='devs@infoxchange.net.au',
     packages=find_packages(),
-    description='Bunch of tools useful to all IX Django projects.',
+    url='http://pypi.python.org/pypi/IXDjango/',
+    license='MIT',
+    description='Management commands for deploying Django projects.',
     long_description=open('README').read(),
     install_requires=[
         "Django >= 1.3.0",
