1bde81385e31e1530615882f0e4d4f1da4311ce1
Alex Hutton <ahutton@infoxchange.net.au>
2013-06-26 12:24:56 +1000

    Test restructuring, ref #16190
    Created ixdjango/tests and put __init__.py inside it.
    
    __init__.py contains what was perviously inside ixdjango/tests.py .
    
    It also contains the contents of qipps's qipps_project/test_runner.py .
    TestRunner can now be used by other projects.

---
+++ ixdjango/tests/__init__.py
@@ -0,0 +1,54 @@
+"""
+Hook into the test runner
+"""
+
+import subprocess
+
+from django.test.simple import DjangoTestSuiteRunner
+
+class TestRunner(DjangoTestSuiteRunner):
+    """
+    Place where we hook into DjangoTestSuiteRunner
+    """
+
+    def setup_test_environment(self, *args, **kwargs):
+        """
+        Hook to set up the test environment
+        """
+
+        from django.conf import settings
+
+        print "Running hooks from %s" % __name__
+
+        username = settings.DATABASES['default']['USER']
+
+        print " - Ensure %s can create a test DB" % username
+        subprocess.call(['sudo', 'su', 'postgres', '-c',
+                         "psql -c 'alter user %s with createdb;'" % username])
+
+        return super(TestRunner, self).setup_test_environment(*args, **kwargs)
+
+"""
+Configures test suite for the package
+"""
+from django.utils import unittest
+
+from ixdjango.test_suite.utils import (
+    CoreUtilsTests
+)
+
+
+def suite():
+    """
+    Put together a suite of tests to run for the application
+    """
+    loader = unittest.TestLoader()
+
+    all_tests = unittest.TestSuite([
+        #
+        # Utilities test cases
+        #
+        loader.loadTestsFromTestCase(CoreUtilsTests)
+    ])
+
+    return all_tests
