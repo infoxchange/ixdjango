273f28a8340f8c994171654809e7f5024a489847
Ricky Cook <ricky@infoxchange.net.au>
2013-10-10 12:16:48 +1100

    Fixed typo in setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import find_packages, setup
 setup(
     name='IXDjango',
     version='0.1.4',
-    author='Infoxchanhe Australia dev team',
+    author='Infoxchange Australia dev team',
     author_email='devs@infoxchange.net.au',
     packages=find_packages(),
     url='http://pypi.python.org/pypi/IXDjango/',
