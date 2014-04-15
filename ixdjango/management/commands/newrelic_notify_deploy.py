1f5d9419e31ef3ae0d0e37bde297435dd77daf01
Ricardo Perez <rperez@infoxchange.net.au>
2014-04-15 11:19:40 +1000

    Changed how the deployment is reported to newrelic

--- ixdjango/management/commands/newrelic_notify_deploy.py
+++ ixdjango/management/commands/newrelic_notify_deploy.py
@@ -5,17 +5,10 @@ Management command to enable New Relic notification of deployments
 
 """
 
-import pwd
 import os
-from subprocess import Popen, PIPE
-from urllib import urlencode
+from subprocess import call, Popen, PIPE
 
-from httplib2 import Http, proxy_info_from_environment
-
-from django.conf import settings
 from django.core.management.base import NoArgsCommand
-
-import newrelic.agent
 
 
 class Command(NoArgsCommand):
@@ -23,42 +16,26 @@ class Command(NoArgsCommand):
     Notify New Relic of the new version
     """
 
-    URL = 'https://rpm.newrelic.com/deployments.xml'
-
     def handle_noargs(self, **options):
-        newrelic.agent.initialize(
-            settings.NEW_RELIC_CONFIG,
-            settings.NEW_RELIC_ENV
-        )
-
-        config = newrelic.agent.global_settings()
-
-        if not config.monitor_mode:
-            return
-
         # get the current git version
         git = Popen(('git', 'describe'), stdout=PIPE)
         ver, _ = git.communicate()
         ver = ver.strip()
 
-        # get the current user
-        user = pwd.getpwuid(os.getuid())
+        # The the tagger name and email
+        git = Popen(('git', 'show', ver), stdout=PIPE)
+        desc, _ = git.communicate()
+        match = re.search('Tagger: (.*) <(.*)>', desc)
+        _, username = match.groups()
 
-        headers = {
-            'x-api-key': settings.NEW_RELIC_API_KEY,
-        }
-        post = {
-            'deployment[app_name]': config.app_name,
-            'deployment[revision]': ver,
-            'deployment[user]': '%s (%s)' % (user.pw_gecos, user.pw_name),
-        }
+        ini_file = './conf/newrelic.ini'
 
         print "Informing New Relic...",
 
-        # post this data
-        http = Http(proxy_info=proxy_info_from_environment())
-        response, _ = http.request(self.URL, 'POST',
-                                   headers=headers,
-                                   body=urlencode(post))
-
-        print response['status']
+        call(['newrelic-admin',
+              'record-deploy',
+              ini_file,
+              ver,  # description
+              ver,  # revision
+              '""',  # changelog
+              username])
