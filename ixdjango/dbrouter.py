e1681eafd49269d8498183c4443e79577ec8663b
Danielle Madeley <dmadeley@infoxchange.net.au>
2014-05-08 15:05:15 +1000

    Fix it better

--- ixdjango/dbrouter.py
+++ ixdjango/dbrouter.py
@@ -10,23 +10,43 @@ class MasterSlaveRouter(object):
     Redirects all reads to standby and all writes to default
     """
 
+    # pylint:disable=unused-argument
+
     MASTER = 'default'
     SLAVE = 'standby'
 
     def db_for_read(self, model, *hints):
+        """
+        Return the DB to use for reads
+        """
+
         return self.SLAVE
 
-    def db_for_write(model, *hints):
+    def db_for_write(self, model, *hints):
+        """
+        Return the DB to use for writes
+        """
+
         return self.MASTER
 
     def allow_relation(self, obj1, obj2, **hints):
-        dbs = set((MASTER, SLAVE))
+        """
+        Determine whether relationships are allowed between the databases
+        (i.e. are they clones who share the same foreign keys)
+        """
 
+        dbs = set((self.MASTER, self.SLAVE))
+
+        # pylint:disable=protected-access
         if obj1._state.db in dbs and \
            obj2._state.db in dbs:
             return True
 
         return None
 
-    def allow_syncdb(self, db, model):
+    def allow_syncdb(self, db, model):  # pylint:disable=invalid-name
+        """
+        Return true if the DB can have it's schema synced
+        """
+
         return db == self.MASTER
