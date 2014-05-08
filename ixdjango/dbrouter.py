4d0abb3a4093ee72ace1fe70670b63b0965670b0
Danielle Madeley <dmadeley@infoxchange.net.au>
2014-05-08 13:50:02 +1000

    Add support for the standby database

---
+++ ixdjango/dbrouter.py
@@ -0,0 +1,32 @@
+"""
+Database router
+"""
+
+
+class MasterSlaveRouter(object):
+    """
+    A master/slave router for IXDjango
+
+    Redirects all reads to standby and all writes to default
+    """
+
+    MASTER = 'default'
+    SLAVE = 'standby'
+
+    def db_for_read(self, model, *hints):
+        return self.SLAVE
+
+    def db_for_write(model, *hints):
+        return self.MASTER
+
+    def allow_relation(self, obj1, obj2, **hints):
+        dbs = set((MASTER, SLAVE))
+
+        if obj1._state.db in dbs and \
+           obj2._state.db in dbs:
+            return True
+
+        return None
+
+    def allow_syncdb(self, db, model):
+        return db == MASTER
