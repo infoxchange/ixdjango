7c033d539f7313d4129bb2e9e1a1934e2929b3b6
Josh Crompton <jcrompton@infoxchange.org>
2016-05-06 16:10:56 +1000

    Bump version to 1.0.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='IXDjango',
-    version='1.0.0',
+    version='1.0.1',
     author='Infoxchange Australia dev team',
     author_email='devs@infoxchange.net.au',
     packages=find_packages(),
